ae2a6af6210ea7fdd7512676cf1d7d6750ea4ee0
engr kashan <kshfkhl@gmail.com>
2025-02-12 01:48:02 +0500

    :fire: added APIs for image uploading

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    protected $fillable = ['user_id', 'company_name', 'company_registration_number', 'company_type', 'company_location'];
+    protected $fillable = ['user_id', 'company_name', 'company_registration_number', 'company_type', 'company_location', 'license_image'];
 
     public function user()
     {
